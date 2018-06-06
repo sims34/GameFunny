bd79dc58cc5905ba405895cffdaa0dae43a67197
sims34 <maxfoerster86@gmail.com>
2018-06-06 22:15:46 +0200

    add shoot class for lesson 11

--- src/com/sims/JavaHub/ShootingBullet/Ship.java
+++ src/com/sims/JavaHub/ShootingBullet/Ship.java
@@ -5,21 +5,9 @@ import java.awt.Graphics;
 
 import javax.swing.JFrame;
 
-public class Ship extends JFrame  {
-	private static final int WIDTH = 400;
-	private static final int HEIGTH = 300;
-	private int x,y,xDirection;
-	public Ship() {
+public class Ship {
+
 	
-		this.x = 175;
-		this.y  = 275;
-	}
-	public void draw(Graphics g){
-		g.setColor(Color.BLUE);
-		g.fillRect(x, y, 40, 10);
-		g.fillRect(x+18, y-7, 4, 7);
-		
-	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
---
+++ src/com/sims/JavaHub/ShootingBullet/Shoot.java
@@ -0,0 +1,71 @@
+package com.sims.JavaHub.ShootingBullet;
+
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+
+import javax.swing.JFrame;
+
+public class Shoot extends JFrame{
+	private Image imgBuffer;
+	private Graphics gphBuffer;
+	static Ship myShip;
+	
+	public Shoot() {
+		inintFrame();
+		addKeyListener(new AL());
+		myShip = new Ship();
+	}
+	
+	//create main frame
+	private void inintFrame(){
+		//frame
+		setSize(450, 300);
+		setTitle("Shoot Ship");
+		setVisible(true);
+		setLocationRelativeTo(null);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+	
+	public void paint(Graphics g){
+		imgBuffer = createImage(getWidth(),getHeight());
+		gphBuffer = imgBuffer.getGraphics();
+		paintComponents(gphBuffer);
+		g.drawImage(imgBuffer, 0, 0, this);
+	}
+	public void paintComponents(Graphics g){
+		//method draw of the ship class
+		
+	}
+	
+	public class AL extends KeyAdapter{
+		public void keyPressed(KeyEvent e){
+			
+		}
+		public void keyReleased(KeyEvent e){
+			
+		}
+	}
+	public static void main(String[] args) {
+		new Shoot();
+
+	}
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
